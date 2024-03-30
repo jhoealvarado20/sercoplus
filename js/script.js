c746be29cf30031db2f36749fd7f46ccc7a2f615
JosephVTX <jvcallupe69@gmail.com>
2024-03-30 17:04:49 -0500

    senati

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-/* const originalSetItem = localStorage.setItem; */
-
-localStorage.setItem = function () {
-  
-    const event = new Event("itemInserted");
-    window.dispatchEvent(event);
-    
-    originalSetItem.apply(this, arguments); 
-};
-
 const addToCartButtons = document.querySelectorAll(".add-to-cart");
 
 addToCartButtons.forEach((button) => {
@@ -56,6 +46,49 @@ function addItemToCart(product) {
   }
 
   localStorage.setItem("cart", JSON.stringify(cart));
+
+  displayCart();
+}
+
+function deleteItemFromCart(name) {
+  let cart = localStorage.getItem("cart");
+  cart = JSON.parse(cart);
+
+  let updatedCart = cart.filter((item) => item.name !== name);
+
+  localStorage.setItem("cart", JSON.stringify(updatedCart));
+
+  displayCart();
+}
+
+function increaseQuantity(name) {
+  let cart = localStorage.getItem("cart");
+  cart = JSON.parse(cart);
+
+  let existingItem = cart.find((item) => item.name === name);
+
+  existingItem.quantity++;
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  displayCart();
+}
+
+function decreaseQuantity(name) {
+  let cart = localStorage.getItem("cart");
+  cart = JSON.parse(cart);
+
+  let existingItem = cart.find((item) => item.name === name);
+
+  if (existingItem.quantity > 1) {
+    existingItem.quantity--;
+  } else {
+    cart = cart.filter((item) => item.name !== name);
+  }
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+
+  displayCart();
 }
 
 function displayCart() {
@@ -74,18 +107,39 @@ function displayCart() {
     const cartItem = document.createElement("div");
     cartItem.classList.add("cart-item");
 
-    cartItem.innerHTML = `
+    cartItem.innerHTML = /*html*/ `
+      <div class="cart-item-info">
         <img src="${item.image}" alt="product" class="cart-item-image" />
         <h3 class="cart-item-name">${item.name}</h3>
-        <h3 class="cart-item-price">${item.price}</h3>
-        <h3 class="cart-item-quantity">${item.quantity}</h3>
-        <button class="remove-button">Remove</button>
-        `;
+      </div>
+      
+      <div class="cart-item-details">
+        <div>
+            <button class="decrease-quantity" onclick="decreaseQuantity('${item.name}')"> <i class="fa-solid fa-minus"></i> </button>
+            <input  class="cart-item-quantity"  value="${item.quantity}" >
+            <button class="increase-quantity" onclick="increaseQuantity('${item.name}')"> <i class="fa-solid fa-plus"></i> </button>
+        </div>
+        <h3 class="cart-item-price">S/ ${item.price}</h3>
+        <button class="remove-item-cart"  onclick="deleteItemFromCart('${item.name}')">
+        <i class="fa-solid fa-trash"></i>
+         </button>
+      </div>
 
+      
+      
+    `;
     shoppingCart.appendChild(cartItem);
   });
 }
 
+const updateCountCart = () => {
+
+   /*  const count = document.getElementsByClassName("cart-item").length
+
+    document.querySelector(".shopping-cart-count").innerHTML = count */
+
+    console.log("updateCountCart");
+}
 
 window.addEventListener("itemInserted", displayCart);
 
